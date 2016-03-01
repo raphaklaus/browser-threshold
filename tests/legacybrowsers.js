a6f6970a7e2cb0a59dc316ab841a92eebe96e6b0
Raphael D. Pinheiro <raphaklaus@gmail.com>
2016-03-01 17:22:14 -0300

    Added semicolons

--- tests/legacybrowsers.js
+++ tests/legacybrowsers.js
@@ -10,27 +10,27 @@ module.exports = {
   },
 
   'IE7: should show threshold' : function (browser) {
-    browser.url('http://localhost:8080/ie.html?ieversion=7').waitForElementVisible('body', 1000)
-    browser.expect.element('#browser-threshold').to.be.present
+    browser.url('http://localhost:8080/ie.html?ieversion=7').waitForElementVisible('body', 1000);
+    browser.expect.element('#browser-threshold').to.be.present;
   },
 
   'IE8: should show threshold' : function (browser) {
-    browser.url('http://localhost:8080/ie.html?ieversion=8').waitForElementVisible('body', 1000)
-    browser.expect.element('#browser-threshold').to.be.present
+    browser.url('http://localhost:8080/ie.html?ieversion=8').waitForElementVisible('body', 1000);
+    browser.expect.element('#browser-threshold').to.be.present;
   },
 
   'IE9: should show threshold' : function (browser) {
-    browser.url('http://localhost:8080/ie.html?ieversion=9').waitForElementVisible('body', 1000)
-    browser.expect.element('#browser-threshold').to.be.present
+    browser.url('http://localhost:8080/ie.html?ieversion=9').waitForElementVisible('body', 1000);
+    browser.expect.element('#browser-threshold').to.be.present;
   },
 
   'IE10: should show threshold' : function (browser) {
-    browser.url('http://localhost:8080/ie.html?ieversion=10').waitForElementVisible('body', 1000)
-    browser.expect.element('#browser-threshold').to.be.present
+    browser.url('http://localhost:8080/ie.html?ieversion=10').waitForElementVisible('body', 1000);
+    browser.expect.element('#browser-threshold').to.be.present;
   },
 
   'IE11: should not show threshold' : function (browser) {
-    browser.url('http://localhost:8080/ie.html?ieversion=11').waitForElementVisible('body', 1000)
-    browser.expect.element('#browser-threshold').to.not.be.present
+    browser.url('http://localhost:8080/ie.html?ieversion=11').waitForElementVisible('body', 1000);
+    browser.expect.element('#browser-threshold').to.not.be.present;
   }  
 };
